91564ee4d5741e72de59540464888d1d917d1ba4
caiquemiranda <caiquemirandas@gmail.com>
2023-03-14 18:50:33 -0300

    cities

--- cities.py
+++ cities.py
@@ -1,8 +1,12 @@
 prompt = "\nPlease enter the name of a city you have visited:"
 prompt += "\n(Enter 'quit' when you are finished.) "
-u while True:
-city = input(prompt)
-if city == 'quit':
-break
-else:
-print("I'd love to go to " + city.title() + "!")
+
+while True:
+    city = input(prompt)
+    
+    if city == 'quit':
+        break
+    
+    else:
+        print("I'd love to go to " + city.title() + "!")
+    
