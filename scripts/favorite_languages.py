0119122c2717490d79f6fcbe7027c7d205938496
caiquemiranda <caiquemirandas@gmail.com>
2023-03-13 21:42:08 -0300

    A List in a Dictionary

---
+++ scripts/favorite_languages.py
@@ -0,0 +1,10 @@
+favorite_languages = {'Jonas': 'python',
+                      'Maria': 'c',
+                      'Marcos': 'ruby',
+                      'Paula': 'python',
+                      }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + \
+        language.title() + ".")
+    
